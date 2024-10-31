11f16c745b08c141d82d8d04a264bccf6031c3ad
Felipe Cesar Sene <felipe.sene@telefonica.com>
2024-10-31 09:54:07 -0300

    tryexcept

--- sabores.py
+++ sabores.py
@@ -1,27 +1,56 @@
 import os
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3.Ativar restaurante')
-print('4. Sair/n')
+def exibir_nome_do_programa():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+    
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-print ('Você escolheu a opção', opcao_escolhida)
-# print(type(opcao_escolhida))
-# print(type(1))
- # opcao_escolhida = int(opcao_escolhida) a partir da variável colocar  como inteiro!
- 
-os.system('cls')
 def finalizar_app():
-  # os.system('clear') no mac
-    print('Finalizar o app\n')
-   
-if opcao_escolhida == 1:
-    print('Cadastrar restaurantes\n')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes\n')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante\n')
-else:
-    finalizar_app
-    print('Finalizar o app\n')
+    os.system('cls')
+    # os.system('clear') 
+    print('Finalizando o app')
+
+def opcao_invalida():
+    print('opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
+
+        if opcao_escolhida == 1: 
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2: 
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3: 
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+if __name__ == '__main__':
+    main()
    
